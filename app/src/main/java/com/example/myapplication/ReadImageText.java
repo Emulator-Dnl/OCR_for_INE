fa0225ca4d377f6ea38a841f4a9994f6c9372694
Daniel Michel <daniel.michel7479@alumnos.udg.mx>
2022-11-22 16:11:20 -0600

    ReadImageText soporta api 21

--- app/src/main/java/com/example/myapplication/ReadImageText.java
+++ app/src/main/java/com/example/myapplication/ReadImageText.java
@@ -8,6 +8,8 @@ import android.util.Log;
 import com.googlecode.tesseract.android.TessBaseAPI;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.StandardCopyOption;
@@ -33,34 +35,54 @@ public class ReadImageText {
             folder.mkdirs();
             Log.d("MainActivity", "se crea path");
         }
-
-        //TODO: Siempre se añade/sobreescribe si la carpeta existe, aún si ya está ahí el archivo de entrenamiento; ineficiente
         if (folder.exists()){
             addFile("spa.traineddata",R.raw.spa_old);
-            //Log.d("MainActivity", "se añade archivo de entrenamiento");
         }
 
         tess.init(dataPath, lang);
     }
 
-    private void addFile (String name, int source) throws IOException {
-        File file = new File(dataPath + "/" + tessDataFolderName + "/" + name);
-        if (!file.exists()){
-
-            InputStream inputStream = context.getResources().openRawResource(source);
-            java.nio.file.Files.copy(
-                    inputStream,
-                    file.toPath(),
-                    StandardCopyOption.REPLACE_EXISTING);
-
-            file.createNewFile();
-            inputStream.close();
-        }
-    }
-
     public String processImage (Bitmap image){
         tess.setImage(image);
         return tess.getUTF8Text();
+    }
+
+    private void addFile (String name, int source) throws IOException {
+        File file = new File(dataPath + "/" + tessDataFolderName + "/" + name);
+
+        //Este if escribe el archivo únicamente en caso de que no exista; no sobreescribe; eficiente
+        if (!file.exists()){
+            Log.d("MainActivity", "se añade archivo de entrenamiento");
+            //InputStream inputStream = context.getResources().openRawResource(source);
+            //java.nio.file.Files.copy(inputStream, file.toPath(), StandardCopyOption.REPLACE_EXISTING);
+            //inputStream.close();
+            //file.createNewFile();
+
+            copyFiletoExternalStorage(source, dataPath + "/" + tessDataFolderName + "/" + name);
+        }
+    }
+
+    private void copyFiletoExternalStorage(int resourceId, String resourceName){
+        String pathSDCard = Environment.getExternalStorageDirectory() + "/Android/data/" + resourceName;
+        try{
+            InputStream in = context.getResources().openRawResource(resourceId);
+            FileOutputStream out = null;
+            out = new FileOutputStream(pathSDCard);
+            byte[] buff = new byte[1024];
+            int read = 0;
+            try {
+                while ((read = in.read(buff)) > 0) {
+                    out.write(buff, 0, read);
+                }
+            } finally {
+                in.close();
+                out.close();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void recycle (){
